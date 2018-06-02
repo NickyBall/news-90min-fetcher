c52934b8a37d8a517cd18d6619351cd759396e81
Jakkrit Junrat <ball.vong.yo.gh@gmail.com>
2018-06-02 21:55:01 +0700

    news fetch from page

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,6 +1,31 @@
 $(document).ready(function() {
     console.log('ready');
-    $.get('/news/fetch', function(data) {
+    // $.get('/news/fetch', function(data) {
+    //     // For each item in our JSON, add a table row and cells to the content string
+        
+    //     // Empty content string
+    //     var tableContent = '';
+    //     $.each(data, function(index) {
+    //         tableContent += '<tr>';
+    //         tableContent += '<td>' + (index+1) + '</td>';
+    //         tableContent += '<td><a href="' + this.link + '">' + this.title + '</a></td>';
+    //         tableContent += '<td>' + this.description + '</td>';
+    //         tableContent += '<td>' + this.author + '</td>';
+    //         tableContent += '<td>' + this.pubDate + '</td>';
+    //         tableContent += '</tr>';
+    //     });
+    //     $('#newstable tbody').html(tableContent);
+    // });
+    fetch_top_news(1);
+    $('#search-form').on('submit', function(e) {
+        e.preventDefault();
+        fetch_top_news($('#page-input').val());
+    });
+    
+});
+
+function fetch_top_news(page) {
+    $.get('/news/top?page='+page, function(data) {
         // For each item in our JSON, add a table row and cells to the content string
         
         // Empty content string
@@ -8,12 +33,12 @@ $(document).ready(function() {
         $.each(data, function(index) {
             tableContent += '<tr>';
             tableContent += '<td>' + (index+1) + '</td>';
-            tableContent += '<td><a href="' + this.link + '">' + this.title + '</a></td>';
-            tableContent += '<td>' + this.description + '</td>';
+            tableContent += '<td><a href="#">' + this.title + '</a></td>';
+            tableContent += '<td>' + this.desc + '</td>';
             tableContent += '<td>' + this.author + '</td>';
-            tableContent += '<td>' + this.pubDate + '</td>';
+            tableContent += '<td><button type="button" class="btn btn-success">Approve</button></td>';
             tableContent += '</tr>';
         });
         $('#newstable tbody').html(tableContent);
-    })
-})
+    });
+}
--- routes/news.js
+++ routes/news.js
@@ -1,21 +1,53 @@
 var express = require('express');
 const axios = require('axios');
+let jsdom = require('jsdom').JSDOM;
 var router = express.Router();
 
 /* GET news listing. */
 router.get('/fetch', function(req, res, next) {
     var parseString = req.parseString;
-    // var xml = "<root>Hello xml2js!</root>";
-    // parseString(xml, function (err, result) {
-    //     console.log(result);
-    // });
+
     axios.get('https://www.90min.com/th/posts.rss').then(response => {
-        // console.log(res.data);
+
         var data = response.data;
         parseString(data, function(err, result) {
-            // console.log(JSON.stringify(result));
             res.json(result.rss.channel[0].item);
         });
+    }).catch(err => {
+        console.log(err);
+    });
+});
+
+/* GET news listing. */
+router.get('/top', function(req, res, next) {
+    var parseString = req.parseString;
+
+    axios.get('https://www.90min.com/th/top-stories?page='+req.query.page).then(response => {
+        var data = response.data;
+        let dom = new jsdom(data);
+        var result = [];
+        var articles = dom.window.document.querySelectorAll('div.feedpage-article');
+        
+        articles.forEach(function(article, index) {
+            var item = {};
+            var img = article.querySelector('img.feedpage-article__thumbnail');
+            item.thumbnail = img.src;
+
+            var data = article.querySelector('div.feedpage-article__data');
+
+            var title = data.querySelector('a.feedpage-article__title');
+            var description = data.querySelector('a.feedpage-article__description');
+            var author = data.querySelector('a.feedpage-article__author-link');
+
+            item.title = title.innerHTML;
+            item.desc = description.innerHTML;
+            item.author = author.innerHTML;
+
+            result.push(item);
+        });
+        
+        res.json(result);
+
     }).catch(err => {
         console.log(err);
     });
